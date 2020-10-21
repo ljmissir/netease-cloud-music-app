cb11b1f83d53916f97c37a0acca34ce4b902744b
Liaojianming <ljmissira@163.com>
2020-10-21 15:42:14 +0800

    新增虚拟列表，优化长列表滚动

---
+++ src/components/ListView.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="list-view" @scroll="scrollEvent" ref="viewContainer">
+    <div class="virtual-list" :style="{ height: listHeight + 'px' }"></div>
+    <van-list
+      class="list-body"
+      :style="{ transform: `translate3d(0, ${startOffset}px, 0)` }"
+    >
+      <van-cell v-for="item in visibleList" :key="item.id" is-link>
+        <img class="avatar" :src="item.picUrl" alt="" />
+        <span>{{ item.name }}</span>
+      </van-cell>
+    </van-list>
+  </div>
+</template>
+
+<script>
+import request from "@/services";
+import { List, Cell } from "vant";
+import utils from "@/utils";
+const { reactive, toRefs, onMounted, computed, ref } = require("vue");
+
+export default {
+  props: {
+    listData: {
+      required: true,
+      default: () => new Array(),
+      type: Array,
+    },
+  },
+  components: {
+    VanList: List,
+    VanCell: Cell,
+  },
+  setup(props) {
+    const state = reactive({
+      startIndex: 0,
+      endIndex: null,
+      itemHeight: 60,
+      startOffset: 0,
+    });
+
+    const { listData } = toRefs(props);
+
+    const viewContainer = ref(null);
+
+    // 虚拟列表总高度
+    const listHeight = computed(() => listData.value.length * state.itemHeight);
+
+    // 屏幕高度
+    const screenHeight = computed(() => window.innerHeight);
+
+    // 可视区渲染项数
+    const visibleCount = computed(() => {
+      return Math.ceil(screenHeight.value / state.itemHeight);
+    });
+
+    // 可视区渲染数据
+    const visibleList = computed(() => {
+      return listData.value.slice(
+        state.startIndex,
+        Math.min(state.endIndex, listData.value.length)
+      );
+    });
+
+    const scrollEvent = utils.debounce(() => {
+      // 当前滚动位置
+      const scrollTop = viewContainer.value.scrollTop;
+      // 此时的开始索引
+      state.startIndex = Math.floor(scrollTop / state.itemHeight);
+      // 此时的结束索引
+      state.endIndex = state.startIndex + visibleCount.value;
+      // 滚动时列表的偏移量
+      state.startOffset = scrollTop - (scrollTop % state.itemHeight);
+    }, 100);
+
+    onMounted(() => {
+      state.endIndex = state.startIndex + visibleCount.value;
+    });
+
+    return {
+      ...toRefs(state),
+      scrollEvent,
+      listHeight,
+      viewContainer,
+      visibleList,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.list-view {
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 100px);
+  overflow-y: auto;
+  .virtual-list {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: -1;
+  }
+  .van-list {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    .van-cell {
+      display: flex;
+      align-items: center;
+      height: 120px;
+      .avatar {
+        width: 80px;
+        height: 80px;
+        margin-right: 30px;
+        border-radius: 50%;
+      }
+    }
+  }
+}
+</style>
--- src/router/modules/user.js
+++ src/router/modules/user.js
@@ -3,4 +3,8 @@ export default [
     path: "/user",
     component: () => import("@/views/user/index"),
   },
+  {
+    path: "/account",
+    component: () => import("@/views/account/index"),
+  },
 ];
