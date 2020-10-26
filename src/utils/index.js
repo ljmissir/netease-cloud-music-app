dee15edb6322089a9c35f52f68df2eb96fb7ebce
Liaojianming <ljmissira@163.com>
2020-10-26 18:10:04 +0800

    更新vant-ui版本

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,20 +18,20 @@ const utils = {
   //! 防抖的原理就是：你尽管触发事件，但是我一定在事件触发n秒后才执行，如果n秒内重复触发事件，则重新计时
   debounce(fn, delay) {
     var timer;
-    return function() {
+    return function(...args) {
       timer && clearTimeout(timer);
       timer = setTimeout(() => {
-        fn.apply(this, arguments);
+        fn.apply(this, args);
       }, delay);
     };
   },
   // ! 节流原理：规定在一个单位时间内，只能执行一次函数。如果这个单位时间内触发多次函数，只有一次生效，稀释事件执行频率
   throttle(fn, delay) {
     var pre = 0;
-    return function() {
+    return function(...args) {
       var now = new Date().getTime();
       if (now - pre > delay) {
-        fn.apply(this, arguments);
+        fn.apply(this, args);
         pre = now;
       }
     };
